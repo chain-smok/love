d70ad65d646aed8b86a6fd0b511fcb3da0153501
chain-smok <101109105+chain-smok@users.noreply.github.com>
2022-04-11 00:26:39 +0800

    Add files via upload

---
+++ map.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int m,n;
+  string s;
+  cin>>m>>n;
+  map<string,int>hay;
+  while(m--)
+  {
+    int p;
+    cin>>s>>p;
+    hay[s]=p;
+  }
+    while(n--)
+  {
+     int ans=0;
+     while(cin>>s&&s!="."){
+     ans+=hay[s];}
+     cout<<ans<<endl;
+  }
+
+
+
+
+}
